cd256377294fb0b94847169de0e1c7e634409947
Erika Tachibana <bana87.tt@icloud.com>
2020-12-05 16:22:44 +0900

    2020年末2021.1休診日更新

--- src/component/homeComponent.tsx
+++ src/component/homeComponent.tsx
@@ -181,19 +181,17 @@ const HomeComponent: React.FC<Props> = (props: Props) => {
                                 <Typography variant="subtitle1">休診は下記を予定しておりますが、新型コロナウイルス感染症の状況により診療日や診療時間に変更が生じる可能性がありますので、ご注意ください。</Typography>
                             </div>
                             <div className={classes.tabBody}>
-                                <Typography variant="h6">2020年10月</Typography>
-                                <Typography variant="subtitle1">10月7日(水)</Typography>
-                                <Typography variant="subtitle1">10月10日(土)</Typography>
-                                <Typography variant="subtitle1">10月17日(土)</Typography>  
-                            </div>
-                            <div className={classes.tabBody}>
-                                <Typography variant="h6">2020年11月</Typography>
-                                <Typography variant="subtitle1">休診の予定はありません。</Typography>  
-                            </div>
-                            <div className={classes.tabBody}>
                                 <Typography variant="h6">2020年12月</Typography>
-                                <Typography variant="subtitle1">12月5日(土)</Typography>  
-                                <Typography variant="subtitle1">※学会出張のため、上記を臨時休診とさせていただいていますが、新型コロナウイルス感染症対策により予定が変更になることがありますので、詳細はお問い合わせください。</Typography>  
+                                <Typography variant="subtitle1">12月5日(土)</Typography>
+                            </div>
+                            <div className={classes.tabBody}>
+                                <Typography variant="h6">年末年始のお知らせ</Typography>
+                                <Typography variant="subtitle1">12月29日(火)〜1月4日(月)まで休診とさせていただきます。</Typography>  
+                                <Typography variant="subtitle1">ご不便をおかけいたしますが、何卒ご了承ください。</Typography>  
+                            </div>
+                            <div className={classes.tabBody}>
+                                <Typography variant="h6">2021年1月</Typography>
+                                <Typography variant="subtitle1">1月23日(土)・午後</Typography>  
                             </div>
                         </TabPanel>
                         <TabPanel value={value} index={1} dir={theme.direction}>
