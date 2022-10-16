43202c801acc181579186c767a8a6e867677a404
Erika Tachibana <bana87.tt@icloud.com>
2022-10-16 23:51:18 +0900

    2022.10-12休診日更新

--- src/component/homeComponent.tsx
+++ src/component/homeComponent.tsx
@@ -182,22 +182,20 @@ const HomeComponent: React.FC<Props> = (props: Props) => {
                                 <Typography variant="subtitle1">詳細は受付までお問い合わせください。</Typography>
                             </div>
                             <div className={classes.tabBody}>
-                                <Typography variant="h6">夏季休暇のお知らせ</Typography>
-                                <Typography variant="subtitle1">8月11日(木・祝)〜15日(月)</Typography>
-                                <Typography variant="subtitle1">上記日程を夏季休暇の為、休診とさせていただきます。</Typography>
-                                <Typography variant="subtitle1">ご不便をおかけいたしますが、よろしくお願いいたします。</Typography>
-                                <Typography variant="subtitle1">　</Typography>  
-                            </div>
-                            <div className={classes.tabBody}>
-                                <Typography variant="h6">2022年9月</Typography>
-                                <Typography variant="subtitle1">現在、休診の予定はありません。</Typography>  
-                                <Typography variant="subtitle1">　</Typography>  
-                            </div>
-                            <div className={classes.tabBody}>
                                 <Typography variant="h6">2022年10月</Typography>
-                                <Typography variant="subtitle1">10月7日(金)</Typography>  
-                                <Typography variant="subtitle1">10月8日(土)</Typography>  
-                                <Typography variant="subtitle1">※10月6日(木)〜10月10日(月・祝)は連休になりますのでご注意ください。</Typography>  
+                                <Typography variant="subtitle1">10月7日(金)</Typography>
+                                <Typography variant="subtitle1">10月8日(土)</Typography>
+                                <Typography variant="subtitle1">※10月6日(木)〜10月10日(月・祝)は連休になりますのでご注意ください。</Typography>
+                                <Typography variant="subtitle1">　</Typography>  
+                            </div>
+                            <div className={classes.tabBody}>
+                                <Typography variant="h6">2022年11月</Typography>
+                                <Typography variant="subtitle1">11月5日(土)</Typography>  
+                                <Typography variant="subtitle1">　</Typography>  
+                            </div>
+                            <div className={classes.tabBody}>
+                                <Typography variant="h6">2022年12月</Typography>
+                                <Typography variant="subtitle1">現在、休診の予定はありません。</Typography>  
                                 <Typography variant="subtitle1">　</Typography>  
                             </div>
                         </TabPanel>
